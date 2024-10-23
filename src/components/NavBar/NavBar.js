00bd7df6d533441bec7faf1553a258c25b5ebbc2
hk-gitshub <officially2808@gmail.com>
2024-10-24 01:58:54 +0530

    updated a to lnk

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,27 +1,27 @@
-
+import { Link } from "react-router-dom"; // Import Link from react-router-dom
 import "./NavBar.css";
 
-function NavBar(){
-    return(
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="/todoAndNote-keeper">Utility Apps</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/todo">To Do</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/notes">Note</a>
-        </li> 
-      </ul>
-    </div>
-  </div>
-</nav>
-    );
+function NavBar() {
+  return (
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="/">Utility Apps</Link> {/* Use Link with the correct 'to' prop */}
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link className="nav-link active" aria-current="page" to="/todo">To Do</Link> {/* Use Link here */}
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link active" aria-current="page" to="/notes">Note</Link> {/* Use Link here */}
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
 export default NavBar;
